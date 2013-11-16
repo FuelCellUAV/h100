1cb3716892161a6fb40b3295ab074cd51a1ad61d
Simon Howroyd <howroydlsu@gmail.com>
2013-11-16 09:04:36 +0000

    restructured folders, added GNU GPL
    # Copyright (C) 2013  Simon Howroyd
    #
    #     This program is free software: you can redistribute it and/or
    modify
    #     it under the terms of the GNU General Public License as published
    by
    #     the Free Software Foundation, either version 3 of the License, or
    #     (at your option) any later version.
    #
    #     This program is distributed in the hope that it will be useful,
    #     but WITHOUT ANY WARRANTY; without even the implied warranty of
    #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    #     GNU General Public License for more details.
    #
    #     You should have received a copy of the GNU General Public License
    #     along with this program.  If not, see
    <http://www.gnu.org/licenses/>.

--- controller.py
+++ controller.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python2
 
+# Fuel Cell Controller for the Horizon H100
+
+# Copyright (C) 2013  Simon Howroyd
+# 
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Import libraries
 import sys
 from   time import time
@@ -105,6 +122,11 @@ print("\nFuel Cell Controller")
 print("Horizon H-100 Stack")
 print("(c) Simon Howroyd 2013")
 print("Loughborough University\n")
+
+print("controller  Copyright (C) 2013  Simon Howroyd")
+print("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.")
+print("This is free software, and you are welcome to redistribute it,")
+print("under certain conditions; type `show c' for details.")
 
 # Main
 while (True):
