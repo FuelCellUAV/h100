c4ea788df7756d9fb1a61607cca3d9afeca4eb1d
Simon Howroyd <howroydlsu@gmail.com>
2014-11-04 20:45:18 +0000

    I2C exception handler

--- adc/adcpi.py
+++ adc/adcpi.py
@@ -58,42 +58,52 @@ class AdcPi2:
     # Method to change the channel we wish to read from
     @staticmethod
     def __changechannel(config):
-        # Using the I2C databus...
-        with I2CMaster(1) as master:
-            master.transaction(
-                writing_bytes(config[0], config[1]))
+        try:
+            # Using the I2C databus...
+            with I2CMaster(1) as master:
+                master.transaction(
+                    writing_bytes(config[0], config[1]))
+                    
+        # If I2C error return
+        except IOError:
+            return
 
     # Method to read adc
     @staticmethod
     def __getadcreading(config, multiplier, res):
-        # Using the I2C databus...
-        with I2CMaster(1) as master:
-            # Calculate how many bytes we will receive for this resolution
-            numBytes = int(max(0, res / 2 - 8) + 3)
-
-            # Initialise the ADC
-            adcreading = master.transaction(
-                writing_bytes(config[0], config[1]),
-                reading(config[0], numBytes))[0]
-
-            # Wait for valid data **blocking**
-            while (adcreading[-1] & 128):
+        try:
+            # Using the I2C databus...
+            with I2CMaster(1) as master:
+                # Calculate how many bytes we will receive for this resolution
+                numBytes = int(max(0, res / 2 - 8) + 3)
+    
+                # Initialise the ADC
                 adcreading = master.transaction(
                     writing_bytes(config[0], config[1]),
                     reading(config[0], numBytes))[0]
-
-            # Shift bits to product result
-            if numBytes is 4:
-                t = ((adcreading[0] & 0b00000001) << 16) | (adcreading[1] << 8) | adcreading[2]
-            else:
-                t = (adcreading[0] << 8) | adcreading[1]
-
-            # Check if positive or negative number and invert if needed
-            if adcreading[0] > 128:
-                t = ~(0x020000 - t)
-
-            # Return result
-            return t * multiplier
+    
+                # Wait for valid data **blocking**
+                while (adcreading[-1] & 128):
+                    adcreading = master.transaction(
+                        writing_bytes(config[0], config[1]),
+                        reading(config[0], numBytes))[0]
+    
+                # Shift bits to product result
+                if numBytes is 4:
+                    t = ((adcreading[0] & 0b00000001) << 16) | (adcreading[1] << 8) | adcreading[2]
+                else:
+                    t = (adcreading[0] << 8) | adcreading[1]
+    
+                # Check if positive or negative number and invert if needed
+                if adcreading[0] > 128:
+                    t = ~(0x020000 - t)
+    
+                # Return result
+                return t * multiplier
+                
+            # If I2C error return error code -1
+            except IOError:
+                return -1
 
     # External getter - call this to receive data
     def get(self, channel):
