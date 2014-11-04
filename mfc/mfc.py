31f7bdf0fc007f7750c60d330049ae396ef5f09f
Simon Howroyd <howroydlsu@gmail.com>
2014-11-04 14:45:40 +0000

    Comments

--- mfc/mfc.py
+++ mfc/mfc.py
@@ -1,37 +1,59 @@
-#!/usr/bin/env python3
-# Simon Howroyd 2014 for python3
-#
-# Requires quick2wire
-# I2C API depends on I2C support in the kernel
+##!/usr/bin/env python3
 
+# Mass Flow Controller Arduino driver
 
+# Copyright (C) 2014  Simon Howroyd
+# 
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#############################################################################
+
+# Import libraries
 from time import sleep
 import quick2wire.i2c as i2c
 
-
+# Define class
 class mfc:
+    # Code to run when class is created
     def __init__(self, address=0x2c):
         self.__address = address
 
-    # Method to read adc
+    # Method to read flow rate from the Arduino
     @staticmethod
     def __get(address):
+        mfcreading = -1
+        
+        # Using the I2C databus...
         with i2c.I2CMaster() as bus:
             mfcreading = bus.transaction(
                 i2c.reading(address, 2))[0]
-            # Return result
-            return mfcreading
+            
+        # Return result
+        return mfcreading
 
     # External getter
     def get(self):
+        # Try and get the flow rate from the Arduino
         try:
             data = self.__get(self.__address)
+            
+        # If that fails (disconnected) return -1, the error code
         except Exception as e:
             return -1
+            
+        # Assemble the two 8bit words to one 16bit integer
         result = data[1] | (data[0] << 8)
-        return result/1000.0  # ml to l
-
-    # Print all channels to screen
-    def printall(self):
-        print("mfc: %02f" % self.get(), end='\t')
-        print()
+        
+        # Return result in litres/min
+        return result/1000.0
